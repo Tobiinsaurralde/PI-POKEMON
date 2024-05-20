99161cba4ddb3f50225acb2a72aca80a6b18e14f
Tobias <tinsaurralde17@gmail.com>
2024-05-20 19:16:01 -0300

    actualizacion index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const PORT = 3002;
+require('dotenv').config();
+const { PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log(`Server listening at ${PORT}`); 
+    console.log('Server listening at', process.env.PORT); 
   });
 });
